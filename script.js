1b9403e209822559cac62555045ff22be6452594
mothana404 <https://github.com/mothana404>
2023-10-01 16:03:19 +0300

    loop update

--- script.js
+++ script.js
@@ -1,15 +1,25 @@
 // EX1
 let theName = window.prompt("Enter you name", "");
-let gender = window.prompt("your gender", "");
+let gender;
 
-if (gender == "male"){
-    window.alert(`Welcome Mr ${theName}`)
-} else if (gender == "female"){
-    window.alert(`Welcome Ms ${theName}`)
-} else
-    window.alert(`Welcome ${theName}`);
+while (gender != "male" || gender != "female"){
+    gender = window.prompt("what is your gender", "");
+    let check = gender.toLocaleLowerCase();
+    if (check === "male"){
+        window.alert(`Welcome Mr ${theName}`);
+        break;
+    } else if (check === "female") {
+        window.alert(`Welcome Ms ${theName}`);
+        break;
+    }
+}
 
 let hotCold = window.prompt("do you want it hot or cold", "");
 let drink = window.prompt("what is your drink", "");
-(hotCold != "" && drink != "") ? console.log("your drink is gitting prepared") : console.log("try again please");
-console.log(`${theName} - ${drink}`);
+(hotCold != "" && drink != "") ? window.alert("your drink is gitting prepared") : window.alert("try again please");
+console.log(`${theName} - ${hotCold} - ${drink}`);
+
+let userAr = [theName, gender, hotCold, drink];
+for (let i = 0; i < userAr.length; i++){
+    console.log(userAr[i]);
+}
