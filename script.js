7e109d47fb43e5088f3e24c21c5f265cb2ccdb95
mothana404 <https://github.com/mothana404>
2023-10-02 19:16:58 +0300

    mybranche

--- script.js
+++ script.js
@@ -1,16 +1,20 @@
+function askGender (gender){
+    while (gender != "male" || gender != "female"){
+        var gender = window.prompt("what is your gender", "");
+        if (gender.toLocaleLowerCase() === "male"){
+            window.alert(`Welcome Mr ${theName}`);
+            break;
+        } else if (gender.toLocaleLowerCase() === "female") {
+            window.alert(`Welcome Ms ${theName}`);
+            break;
+        }
+    }
+    return gender;
+}
+
 // EX1
 let theName = window.prompt("Enter you name", "");
-
-while (gender != "male" || gender != "female"){
-    var gender = window.prompt("what is your gender", "");
-    if (gender.toLocaleLowerCase() === "male"){
-        window.alert(`Welcome Mr ${theName}`);
-        break;
-    } else if (gender.toLocaleLowerCase() === "female") {
-        window.alert(`Welcome Ms ${theName}`);
-        break;
-    }
-}
+let gender = askGender();
 
 let hotCold = window.prompt("do you want it hot or cold", "");
 let drink = window.prompt("what is your drink", "");
@@ -21,4 +25,3 @@ let userAr = [theName, gender, hotCold, drink];
 for (let i = 0; i < userAr.length; i++){
     console.log(userAr[i]);
 }
-//kinbkjbkbikikvkv
