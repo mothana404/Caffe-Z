6fb7ebb161a559b79e85d658b02aebe698fc15f8
mothana404 <https://github.com/mothana404>
2023-10-03 16:03:55 +0300

    dom

--- script.js
+++ script.js
@@ -25,3 +25,24 @@ let userAr = [theName, gender, hotCold, drink];
 for (let i = 0; i < userAr.length; i++){
     console.log(userAr[i]);
 }
+
+let age = window.prompt("what is your age","");
+
+let dev = document.getElementById("speDiv");
+let par = document.createElement("p");
+let ul = document.createElement("ul");
+let li1 = document.createElement("li");
+let li2 = document.createElement("li");
+let li3 = document.createElement("li");
+
+par.innerHTML = theName;
+li1.innerHTML = `gender ${gender}`;
+li2.innerHTML = `Age ${age}`;
+li3.innerHTML = `drink : ${drink}`;
+ul.appendChild(li1);
+ul.appendChild(li2);
+ul.appendChild(li3);
+
+dev.appendChild(par);
+dev.appendChild(ul);
+document.body.appendChild(dev);
