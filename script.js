2704d9b06a0e0767d3da952a74cd69162b886e24
mothana404 <https://github.com/mothana404>
2023-10-03 20:32:59 +0300

    eventTask

--- script.js
+++ script.js
@@ -1,32 +1,32 @@
-function askGender (gender){
-    while (gender != "male" || gender != "female"){
-        var gender = window.prompt("what is your gender", "");
-        if (gender.toLocaleLowerCase() === "male"){
-            window.alert(`Welcome Mr ${theName}`);
-            break;
-        } else if (gender.toLocaleLowerCase() === "female") {
-            window.alert(`Welcome Ms ${theName}`);
-            break;
-        }
-    }
-    return gender;
-}
+// function askGender (gender){
+//     while (gender != "male" || gender != "female"){
+//         var gender = window.prompt("what is your gender", "");
+//         if (gender.toLocaleLowerCase() === "male"){
+//             window.alert(`Welcome Mr ${theName}`);
+//             break;
+//         } else if (gender.toLocaleLowerCase() === "female") {
+//             window.alert(`Welcome Ms ${theName}`);
+//             break;
+//         }
+//     }
+//     return gender;
+// }
 
-// EX1
-let theName = window.prompt("Enter you name", "");
-let gender = askGender();
+// // EX1
+// let theName = window.prompt("Enter you name", "");
+// let gender = askGender();
 
-let hotCold = window.prompt("do you want it hot or cold", "");
-let drink = window.prompt("what is your drink", "");
-(hotCold != "" && drink != "") ? window.alert("your drink is gitting prepared") : window.alert("try again please");
-console.log(`${theName} - ${hotCold} - ${drink}`);
+// let hotCold = window.prompt("do you want it hot or cold", "");
+// let drink = window.prompt("what is your drink", "");
+// (hotCold != "" && drink != "") ? window.alert("your drink is gitting prepared") : window.alert("try again please");
+// console.log(`${theName} - ${hotCold} - ${drink}`);
 
-let userAr = [theName, gender, hotCold, drink];
-for (let i = 0; i < userAr.length; i++){
-    console.log(userAr[i]);
-}
+// let userAr = [theName, gender, hotCold, drink];
+// for (let i = 0; i < userAr.length; i++){
+//     console.log(userAr[i]);
+// }
 
-let age = window.prompt("what is your age","");
+// let age = window.prompt("what is your age","");
 
 let dev = document.getElementById("speDiv");
 let par = document.createElement("p");
@@ -35,10 +35,23 @@ let li1 = document.createElement("li");
 let li2 = document.createElement("li");
 let li3 = document.createElement("li");
 
-par.innerHTML = theName;
-li1.innerHTML = `gender ${gender}`;
-li2.innerHTML = `Age ${age}`;
-li3.innerHTML = `drink : ${drink}`;
+let theName = document.querySelector("input[name = name]");
+let age = document.querySelector("input[name = age]");
+let drink = document.querySelector("input[name = drink]");
+
+let button = document.querySelector("#send");
+button.addEventListener("click", function (e){
+    e.preventDefault();
+    par.innerHTML = theName.value;
+    li1.innerHTML = age.value;
+    if (document.querySelector("input[name = hot]").checked){
+        li2.innerHTML = "hot";
+    } else if (document.querySelector("input[name = cold]").checked){
+        li2.innerHTML = "cold";
+    }
+    li3.innerHTML = drink.value;
+});
+
 ul.appendChild(li1);
 ul.appendChild(li2);
 ul.appendChild(li3);
